2c2da9a00a307fd01484bb2307b1ade82f202af1
_wania <91545143+wwania@users.noreply.github.com>
2024-11-11 23:42:42 +0500

    Update cl_cassie.lua

--- lua/cassie/cl_cassie.lua
+++ lua/cassie/cl_cassie.lua
@@ -4,8 +4,7 @@
 	
 	Copyright (c) Ivan Malakhov <malahoff21@gmail.com>
 	
-	For the full copyright and license information, please view the LICENSE
-	file that was distributed with this source code.
+	For the full copyright and license information, please view the LICENSE file that was distributed with this source code.
 
 ]]
 
